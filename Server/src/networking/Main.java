df3b86849a68e65de598899cdc04d6cd3fa62c4d
bwielk <bwielk@gmail.com>
2018-06-09 00:33:01 +0200

    Created two separate projects. Created the server

---
+++ Server/src/networking/Main.java
@@ -0,0 +1,30 @@
+package networking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (ServerSocket serviceSocket = new ServerSocket(5000)){
+            Socket socket = serviceSocket.accept();
+            System.out.println("Client connected");
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true){
+                String echoString = input.readLine();
+                if(echoString.equals("exit")){
+                    break;
+                }
+                output.println("Echo from server => " + echoString);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
