1801ed575038666534fa5f6adf112f74499622a2
bwielk <bwielk@gmail.com>
2018-06-10 22:02:36 +0100

    Code refactor. Added multithreading for multiple clients

---
+++ Server/src/networking/Echoer.java
@@ -0,0 +1,39 @@
+package networking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Echoer extends Thread{
+
+    private Socket socket;
+
+    public Echoer(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try{
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream());
+            while(true){
+                String echoString = input.readLine();
+                if(echoString.equals("exit")){
+                    break;
+                }
+                output.println(echoString);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                socket.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- Server/src/networking/Main.java
+++ Server/src/networking/Main.java
@@ -11,17 +11,8 @@ public class Main {
 
     public static void main(String[] args) {
         try (ServerSocket serviceSocket = new ServerSocket(5000)){
-            Socket socket = serviceSocket.accept();
-            System.out.println("Client connected");
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while(true){
-                String echoString = input.readLine();
-                if(echoString.equals("exit")){
-                    break;
-                }
-                output.println("Echo from server => " + echoString);
+                new Echoer(serviceSocket.accept()).start();
             }
         }catch(IOException e){
             System.out.println("Server error");
